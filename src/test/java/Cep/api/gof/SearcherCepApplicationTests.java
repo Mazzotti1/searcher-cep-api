d79a297fae5ee711cc0abab6294e17616ba9a05d
Gabriel Mazzotti <gmazzottidesouza@gmail.com>
2023-03-14 10:24:45 -0300

    Created api to search adress with CEP

---
+++ src/test/java/Cep/api/gof/SearcherCepApplicationTests.java
@@ -0,0 +1,13 @@
+package Cep.api.gof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearcherCepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
